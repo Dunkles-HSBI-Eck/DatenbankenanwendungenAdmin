cc2c8ddccbe058971caa44ce9024d409a2fb3128
Hicks-99 <94490389+Hicks-99@users.noreply.github.com>
2025-06-25 10:01:36 +0200

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,17 @@
 	let { children } = $props();
 </script>
 
+<div class="sticky top-0 left-0 w-full flex justify-center bg-blue-500 h-16">
+    <ol class="flex items-center space-x-4">
+        <li>
+            <a href="/" class="hover:underline">Home</a>
+        </li>
+        <li>
+            <a href="/movies" class="hover:underline">Filme</a>
+        </li>
+        <li>
+            <a href="/requests" class="hover:underline">Anfragen</a>
+        </li>
+    </ol>
+</div>
 {@render children()}
