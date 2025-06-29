c7dc5be902260b1be1a273379e96d72d26c74c1a
Julius Reuter <jureuter06@gmail.com>
2025-06-29 14:19:07 +0200

    implemented file Upload for createMovie page

--- src/routes/createMovie/+page.svelte
+++ src/routes/createMovie/+page.svelte
@@ -4,24 +4,39 @@
     let title = $state();
     let description = $state();
     let price = $state();
-    let coverFile = $state();
-    let bannerFile = $state();
-    let videoFile = $state();
+    let coverFile = null;
+    let bannerFile = null;
+    let videoFile = null;
     let currentVideoPath = $state();
+
+    let prev = $state();
 
     function UploadCover(event)
     {
-
+        const files = event.files;
+        console.log(files);
+        if(!files || files.length === 0){ return};
+        coverFile = files[0];
+        console.log(coverFile);
     }
     
     function UploadBanner(event)
     {
-
+      
+        const files = event.files;
+        console.log(files);
+        if(!files || files.length === 0){ return};
+        bannerFile = files[0];
+        console.log(bannerFile);
     }
     
     function UploadVideo(event)
     {
-
+        const files = event.files;
+        console.log(files);
+        if(!files || files.length === 0){ return};
+        videoFile = files[0];
+        console.log(videoFile);
     }
 
     function submit()
@@ -43,7 +58,7 @@
     <div class="">
         <label class="label">
           <span class="label-text">Description</span>
-          <textarea class="textarea" bind:value={title} rows="4" placeholder="Enter description here..."></textarea>
+          <textarea class="textarea" bind:value={description} rows="4" placeholder="Enter description here..."></textarea>
         </label>
     </div>
     <br>
@@ -60,21 +75,21 @@
     <br>
     <div class="flex ">
         <span class="mr-10">cover</span>
-        <FileUpload name="example" accept="image/*" maxFiles={1} onFileChange={console.log} onFileReject={console.error} classes="w-fit border " />
+        <FileUpload name="example" accept="image/*" maxFiles={1} onFileReject={console.error} classes="w-fit border " onFileAccept={(e) => UploadCover(e)} />
     </div>
     <br>
     <hr class="hr" />
     <br>
     <div class="flex ">
         <span class="mr-10">banner</span>
-        <FileUpload name="example" accept="image/*" maxFiles={1} onFileChange={console.log} onFileReject={console.error} classes="w-fit border " />
+        <FileUpload name="example" accept="image/*" maxFiles={1} onFileReject={console.error} classes="w-fit border " onFileAccept={UploadBanner} />
     </div>
     <br>
     <hr class="hr" />
     <br>
     <div class="flex ">
         <span class="mr-10">Video File</span>
-        <FileUpload name="example" accept="video/*" maxFiles={1} onFileChange={console.log} onFileReject={console.error} classes="w-fit border " />
+        <FileUpload name="example" accept="video/*" maxFiles={1} onFileReject={console.error} classes="w-fit border " onFileAccept={UploadVideo}/>
     </div>
     <div class="">
         <label class="label">
@@ -87,6 +102,5 @@
     <hr class="hr" />
     <br>
 
-    <button class="w-fit bg-blue-400 p-1 rounded">submit</button>
-
+    <button class="w-fit bg-blue-400 p-1 rounded" onclick={submit}>submit</button>
 </div>
